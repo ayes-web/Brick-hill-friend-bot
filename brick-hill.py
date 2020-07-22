cb89cddbe4b5c47e57d2e98e6e7a303408144396
ayes-web <52951851+ayes-web@users.noreply.github.com>
2020-07-23 00:10:24 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- brick-hill.py
+++ brick-hill.py
@@ -4,7 +4,7 @@ import json, requests, time, random
 # Browser used by the bot
 driver = webdriver.Firefox()
 waitTime = [10,11,12,13,14,15]
-firstAccount = 353972
+firstAccount = 354008
 
 # You have to login or else it doesn't work
 driver.get("https://www.brick-hill.com/login")
@@ -26,26 +26,36 @@ while firstAccount != -666:
 
 		g = 0
 		print("Empty page found!")
-		while errorMessage.text == "Error 404: Page Not Found" or g == 0:
-			print("Waiting 30 seconds!")
-			time.sleep(30)
-
-			driver.get("https://www.brick-hill.com/user/" + str(firstAccount))
-
+		try:
 			try:
-				# checks for error message
-				errorMessage = driver.find_element_by_xpath("//div[@class='main-holder grid']/div[2]/span")
+				while errorMessage.text == "Error 404: Page Not Found" or g == 0:
+					print("Waiting 30 seconds!")
+					time.sleep(30)
+
+					driver.get("https://www.brick-hill.com/user/" + str(firstAccount))
+
+					try:
+						# checks for error message
+						errorMessage = driver.find_element_by_xpath("//div[@class='main-holder grid']/div[2]/span")
+					except:
+						print("New account found!")
+						g = 1
+						#gets error here
 			except:
-				print("New account found!")
-				g = 1
-	print("Username: " + name.text + " ID: " + str(firstAccount))
+				print("FUck you it crashed ahahaah")
+		except:
+			print("HAHAHAHAHAHH")
+	try:
+		print("Username: " + name.text + " ID: " + str(firstAccount))
+	except:
+		print("I failed to send you the username and id :(")
 
 	try:
 		# Finds the friend request button
 		button = driver.find_element_by_xpath("//a[@class='button small inline blue']")
 		button.click()
 	except:
-		print("I can't find the button or i have already sent friend request to them.")
+		print("I already sent friend request to them.")
 		print("Going to the next one!")
 
 	firstAccount += 1
