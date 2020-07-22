5ef2436e34919bb708366ea27f7175750665f8fa
ayes-web <52951851+ayes-web@users.noreply.github.com>
2020-07-23 00:43:18 +0300

    It somehow works now
    I DONT KNOW HOW also i made the times better haha

--- brick-hill.py
+++ brick-hill.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
-import json, requests, time, random
+import json, requests, time
+from random import *
 
 # Browser used by the bot
 driver = webdriver.Firefox()
-waitTime = [10,11,12,13,14,15]
-firstAccount = 354008
+waitTime = [10,15]
+firstAccount = 354022
 
 # You have to login or else it doesn't work
 driver.get("https://www.brick-hill.com/login")
 t = input("Please press enter after you have logged in to the site! ")
 print(" ")
 
+#loops infinite
 while firstAccount != -666:
 	driver.get("https://www.brick-hill.com/user/" + str(firstAccount))
 
@@ -27,6 +29,7 @@ while firstAccount != -666:
 		g = 0
 		print("Empty page found!")
 		try:
+			# i have no idea why does trys are needed but it closes otherwise please help i am going insane
 			try:
 				while errorMessage.text == "Error 404: Page Not Found" or g == 0:
 					print("Waiting 30 seconds!")
@@ -40,14 +43,20 @@ while firstAccount != -666:
 					except:
 						print("New account found!")
 						g = 1
-						#gets error here
 			except:
 				print("FUck you it crashed ahahaah")
 		except:
 			print("HAHAHAHAHAHH")
+
 	try:
+		# prints username and id
 		print("Username: " + name.text + " ID: " + str(firstAccount))
 	except:
+		#tries to get the name again
+		driver.get("https://www.brick-hill.com/user/" + str(firstAccount))
+		name = driver.find_element_by_xpath("//div[@class='content text-center bold medium-text relative ellipsis']//span[@class='ellipsis']")
+		print("Username: " + name.text + " ID: " + str(firstAccount))
+		# username fail message
 		print("I failed to send you the username and id :(")
 
 	try:
@@ -55,11 +64,15 @@ while firstAccount != -666:
 		button = driver.find_element_by_xpath("//a[@class='button small inline blue']")
 		button.click()
 	except:
+		# if it doesnt find the button it sends this and doesnt click
 		print("I already sent friend request to them.")
 		print("Going to the next one!")
 
 	firstAccount += 1
-	chosenWait = random.choice(waitTime)
+
+	#gives random wait time
+	chosenWait = uniform(waitTime[0], waitTime[1])
 	print("Waiting " + str(chosenWait) + " Seconds")
+	#waits the given time
 	time.sleep(chosenWait)
 	print(" ")
