90dbc81524a12030f701f5989f671406ad9915e5
ayes-web <52951851+ayes-web@users.noreply.github.com>
2020-07-22 21:37:46 +0300

    Update brick-hill.py

--- brick-hill.py
+++ brick-hill.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-import json, requests, time
+import json, requests, time, random
 
 # Browser used by the bot
 driver = webdriver.Firefox()
-waitTime = 10
+waitTime = [5,6,7,8,9,10]
 
 # You have to login or else it doesn't work
 driver.get("https://www.brick-hill.com/login")
@@ -12,27 +12,30 @@ print(" ")
 
 # I don't really know where to get the account count 
 # so i just went to the newest account and put that number :D
-lastAccount = 353863
+lastAccount = 353913
 firstAccount = -1
 
 while firstAccount <= lastAccount:
-	driver.get("https://www.brick-hill.com/user/" + str(firstAccount))
+	driver.get("https://www.brick-hill.com/user/" + str(lastAccount))
 
 	try:
 		# Finds the username
 		name = driver.find_element_by_xpath("//div[@class='content text-center bold medium-text relative ellipsis']//span[@class='ellipsis']")
-		print("Username: " + name.text + " ID: " + str(firstAccount))
+		print("Username: " + name.text + " ID: " + str(lastAccount))
 	except: 
 		print("I can't find the username!")
-		print("ID: " + str(firstAccount))
+		print("ID: " + str(lastAccount))
 
 
 	try:
 		# Finds the friend request button
 		button = driver.find_element_by_xpath("//a[@class='button small inline blue']")
+		button.click()
 	except:
 		print("I can't find the button, going to the next one!")
 
-	firstAccount += 1
-	time.sleep(waitTime)
+	lastAccount -= 1
+	chosenWait = random.choice(waitTime)
+	print("Waited " + str(chosenWait) + " Seconds")
+	time.sleep(chosenWait)
 	print(" ")
