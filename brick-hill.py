f9fef8b71d725a78e30011dc0b2dc2a1e721528d
ayes-web <52951851+ayes-web@users.noreply.github.com>
2020-07-23 23:21:54 +0300

    Doesnt load page if it has already friended it

--- brick-hill.py
+++ brick-hill.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
-import json, requests, time, getpass
+import json, requests, time, getpass, os
 from random import *
+from selenium import webdriver
 
 # how long it waits going to next user
 waitTime1 = [10,15]
@@ -12,10 +12,22 @@ waitTime2 = 60
 startingID =  354225
 currentID = ""
 
+#gets the username and password so it can login to account
 username = input("Enter your username: ")
 password = getpass.getpass("Enter your password: ")
 print(" ")
 
+
+# stores all of the people you have friended so its easier to skip later
+# has different file for different accounts
+try:
+    with open("accounts/" + username + ".txt") as f:
+    	print("Found accounts.txt!")
+except IOError:
+    print("No accounts.txt found, creating one!")
+    f = open("accounts/" + username + ".txt", "x")
+
+#chels if input is number
 def is_number(s):
     try:
         int(s)
@@ -26,16 +38,19 @@ def is_number(s):
 print("Enter the id to start from")
 print("Nothing for default")
 
+#checks if input is number or empty
 try:
 	currentID = int(input("ID: "))
 except:
 	if currentID != "":
 		while is_number(currentID) == False:
 			try:
+				# chooses the user input
 				currentID = int(input("ID: "))
 			except:
 				print("Not a number, try again")
 	else:
+		#chooses the default id
 		currentID = startingID
 		print("You chose " + str(currentID))
 
@@ -58,55 +73,71 @@ print(" ")
 
 #loops go brrr
 while True:
-	driver.get("https://www.brick-hill.com/user/" + str(currentID))
+	#gets the username using brick-hill api
+	apiLink = "https://api.brick-hill.com/v1/user/profile?id=" + str(currentID)
+	userInfo = requests.get(apiLink)
+	userName = json.loads(userInfo.text)
 
-	try:
-		# looks for error message
-		errorMessage = driver.find_element_by_xpath("//div[@class='main-holder grid']/div[2]/span")
-		g = 0
-		print("404 page found!")
-		# gets error when it finds new account so this counters it
-		try:
-			while errorMessage.text == "Error 404: Page Not Found" or g == 0:
-				# waits a little bit then tries again
-				print("Waiting " + str(waitTime2) + " seconds!")
-				time.sleep(waitTime2)
+	with open("accounts/" + username + ".txt") as f:
+		if userName["username"] + "\n" in f.read():
+			#if it finds the username in the list it skips
+			print("Skipping " + userName["username"])
+		else:
+			#chooses wait time between pages
+			chosenWait = uniform(waitTime1[0], waitTime1[1])
 
-				driver.get("https://www.brick-hill.com/user/" + str(currentID))
+			#opens user page
+			driver.get("https://www.brick-hill.com/user/" + str(currentID))
 
+			#adds the username to the txt file
+			f = open("accounts/" + username + ".txt", "a")
+			f.write(userName["username"] + "\n")
+			f.close()
+
+			try:
+				# looks for error message
+				errorMessage = driver.find_element_by_xpath("//div[@class='main-holder grid']/div[2]/span")
+				g = 0
+				print("404 page found!")
+				# gets error when it finds new account so this counters it
 				try:
-					# checks for error message
-					errorMessage = driver.find_element_by_xpath("//div[@class='main-holder grid']/div[2]/span")
+					while errorMessage.text == "Error 404: Page Not Found" or g == 0:
+						# waits a little bit then tries again
+						print("Waiting " + str(waitTime2) + " seconds!")
+						time.sleep(waitTime2)
+
+						driver.get("https://www.brick-hill.com/user/" + str(currentID))
+
+						try:
+							# checks for error message
+							errorMessage = driver.find_element_by_xpath("//div[@class='main-holder grid']/div[2]/span")
+						except:
+							#gets out of loop
+							g = 1	
 				except:
-					#gets out of loop
-					g = 1	
-		except:
-			print(" ")
-			print("New account found!")
-	except:
-		print("")
+					print(" ")
+					print("New account found!")
+			except:
+				print("")
 
-	try:
-		# prints username and id
-		driver.get("https://www.brick-hill.com/user/" + str(currentID))
-		name = driver.find_element_by_xpath("//div[@class='content text-center bold medium-text relative ellipsis']//span[@class='ellipsis']")
-		print("Username: " + name.text + " ID: " + str(currentID))
-	except:
-		# username fail message
-		print("I can't send the username and id for some reason!")
+			try:
+				# prints username and id
+				print("Username: " + userName["username"] + " ID: " + str(currentID))
+			except:
+				# username fail message
+				print("I can't send the username and id for some reason!")
 
-	try:
-		# Finds the friend request button
-		button = driver.find_element_by_xpath("//a[@class='button small inline blue']")
-		button.click()
-	except:
-		# if it doesnt find the button it sends this and doesnt click
-		print("I already sent friend request to them. (Or you aren't logged in)")
+			try:
+				# Finds the friend request button
+				button = driver.find_element_by_xpath("//a[@class='button small inline blue']")
+				button.click()
+			except:
+				# if it doesnt find the button it sends this and doesnt click
+				print("I already sent friend request to them. (Or you aren't logged in)")
+				print("Waiting " + str(chosenWait) + " Seconds")
+			else:
+				print("Request sent, waiting " + str(chosenWait) + " Seconds")
+			# sleeps only if it didnt skip a page
+			time.sleep(chosenWait)
 
 	currentID += 1
-
-	#gives random wait time
-	chosenWait = uniform(waitTime1[0], waitTime1[1])
-	print("Request sent, waiting " + str(chosenWait) + " Seconds")
-	#waits the given time
-	time.sleep(chosenWait)
